bde0a149fb92f6a978b350cf371e810706ae18dc
Gauravd70 <gdudhe70@gmail.com>
2026-05-23 17:51:14 +0530

    Removed JWT filter and moved to starter-web

--- commons-spring/src/main/java/com/gauravd70/commons/advices/GlobalControllerAdvice.java
+++ commons-spring/src/main/java/com/gauravd70/commons/advices/GlobalControllerAdvice.java
@@ -4,29 +4,27 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.gauravd70.commons.dtos.GenericResponse;
 import com.gauravd70.commons.exceptions.BadRequestException;
 import com.gauravd70.commons.exceptions.InternalServerException;
 import com.gauravd70.commons.exceptions.UnauthorizedException;
 
-import reactor.core.publisher.Mono;
-
 @RestControllerAdvice
 public class GlobalControllerAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = {InternalServerException.class, Exception.class})
-    public Mono<ResponseEntity<GenericResponse>> handleInternalServerException(Exception e) {
-        return Mono.just(ResponseEntity.internalServerError().body(GenericResponse.builder().message(e.getMessage()).build()));
+    public ResponseEntity<GenericResponse> handleInternalServerException(Exception e) {
+        return ResponseEntity.internalServerError().body(GenericResponse.builder().message(e.getMessage()).build());
     }
 
     @ExceptionHandler(value = {BadRequestException.class})
-    public Mono<ResponseEntity<GenericResponse>> handleBadRequestException(BadRequestException e) {
-        return Mono.just(ResponseEntity.badRequest().body(GenericResponse.builder().message(e.getMessage()).build()));
+    public ResponseEntity<GenericResponse> handleBadRequestException(BadRequestException e) {
+        return ResponseEntity.badRequest().body(GenericResponse.builder().message(e.getMessage()).build());
     }
 
     @ExceptionHandler(value = {UnauthorizedException.class})
-    public Mono<ResponseEntity<GenericResponse>> handleUnauthorizedException(UnauthorizedException e) {
-        return Mono.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(GenericResponse.builder().message(e.getMessage()).build()));
+    public ResponseEntity<GenericResponse> handleUnauthorizedException(UnauthorizedException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(GenericResponse.builder().message(e.getMessage()).build());
     }
 }
--- commons-spring/src/main/java/com/gauravd70/commons/security/JwtType.java
+++
@@ -1,5 +0,0 @@
-package com.gauravd70.commons.security;
-
-public enum JwtType {
-    ACCESS_TOKEN, REFRESH_TOKEN;
-}
