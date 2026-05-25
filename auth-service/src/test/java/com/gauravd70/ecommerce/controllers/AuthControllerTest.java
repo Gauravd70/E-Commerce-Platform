7b3a3c9cdd8907becbad9b3fe22d44dfd9a78955
Gauravd70 <gdudhe70@gmail.com>
2026-05-25 21:39:05 +0530

    Refactoring auth-service

--- auth-service/src/test/java/com/gauravd70/ecommerce/controllers/AuthControllerTest.java
+++ auth-service/src/test/java/com/gauravd70/ecommerce/controllers/AuthControllerTest.java
@@ -1,7 +1,5 @@
 package com.gauravd70.ecommerce.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
