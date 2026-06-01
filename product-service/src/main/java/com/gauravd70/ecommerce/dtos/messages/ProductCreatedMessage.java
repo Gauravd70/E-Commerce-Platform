cb2e85780cad56a71e8a4027bc9bc28b76f19f0c
Gauravd70 <gdudhe70@gmail.com>
2026-06-01 15:42:24 +0530

    Added logic to send product created message to RabbitMQ

---
+++ product-service/src/main/java/com/gauravd70/ecommerce/dtos/messages/ProductCreatedMessage.java
@@ -0,0 +1,19 @@
+package com.gauravd70.ecommerce.dtos.messages;
+
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductCreatedMessage {
+    private String brand;
+    private String model;
+    private Map<String, String> attributes;
+    private CategoryMessage category;
+}
